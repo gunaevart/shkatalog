c385f53929a0acf111b30f0d8e150d9b6255e56e
gunaevart <gunaevart@mail.ru>
2019-04-08 10:14:49 +0300

    Добавление каталога

---
+++ PRF-145/index.php
@@ -0,0 +1,166 @@
+﻿<!DOCTYPE html>
+<html lang="ru">
+   <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <meta name="description" content="Запчасти на сельхозтехнику для Вас по низким ценам от производителя с доставкой по всей России">
+      <meta name="author" content="gunaevart">
+      <title>Каталог Пресс-подборщик ПРФ-145 Цена: 310.000 рублей с НДС</title>
+      <meta name="keywords" content="Запчасти для сельхозтехники,Каталог сельхозтехники, Каталог ПРФ, Запчасти на ПРФ, Каталог ГВР,КТН,пресс подборщик рулонный,Механизм прессующий">
+      <meta name="description" content="Запчасти на сельхозтехнику для Вас по низким ценам Режущий аппарат косилки, вал привода, аппарат обматывающий, привод ПР, пресс подборщик прф, камера передняя прф, камера задняя прф">
+      <link rel="stylesheet" href="../style/style.css">
+      <link rel="stylesheet" href="catalogStyle/catalogStyle.css">
+      ﻿
+      <meta name="google-site-verification" content="OOYs_4xIXfJwLT0tB3s-2Pwm3eCcdkHkLUoKeJsnAzg" />
+      <meta name='wmail-verification' content='31b30beed8af6d4e' />
+      <link rel="icon" href="../../../../favicon.ico">
+      <meta property="og:image" content="path/to/image.jpg">
+      <link rel="shortcut icon" href="img/favicon/favicon.ico" type="image/x-icon">
+      <link rel="apple-touch-icon" href="img/favicon/apple-touch-icon.png">
+      <link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-touch-icon-72x72.png">
+      <link rel="apple-touch-icon" sizes="114x114" href="img/favicon/apple-touch-icon-114x114.png">
+      <!-- Chrome, Firefox OS and Opera -->
+      <meta name="theme-color" content="#000">
+      <!-- Windows Phone -->
+      <meta name="msapplication-navbutton-color" content="#000">
+      <!-- iOS Safari -->
+      <meta name="apple-mobile-web-app-status-bar-style" content="#000">
+      <title>Каталог запчастей на пресс-подборщик ПРФ, ГВР, Косилка КС-21 и другую сельхозтехнику</title>
+      <!-- Bootstrap core CSS -->
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+      <!-- Place your stylesheet here-->
+      <link href="stylesheet.css" rel="stylesheet" type="text/css">
+   </head>
+   <body>
+      <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+         <a class="navbar-brand" href="index.php">Главная</a>
+         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+         <span class="navbar-toggler-icon"></span>
+         </button>
+         <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+            <ul class="navbar-nav mr-auto">
+               <li class="nav-item">
+                  <a class="nav-link" href="../catalog.php">Кaталог</a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link" href="../dostavka.php">Доставка</a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link" href="../about.php">О нас</a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link" href="../prise/">Прайс лист</a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link" href="../site-maps.php">Карта сайта</a>
+               </li>
+               <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Дополнительно</a>
+                  <div class="dropdown-menu" aria-labelledby="dropdown01">
+                     <a class="dropdown-item" href="../dostavka.php">Доставка</a>
+                     <a class="dropdown-item" href="#">Сотруднечество</a>
+                     <a class="dropdown-item" href="#">Обратная связь</a>
+                  </div>
+               </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+               <input class="form-control mr-sm-2" type="text" placeholder="Искать" aria-label="Search">
+               <button class="btn btn-secondary my-2 my-sm-0" type="submit">Искать</button>
+            </form>
+         </div>
+      </nav>
+      <main role="main" class="container">
+      <div class="mt-5 pt-5">
+         <div class="row">
+            <div class="col-md-6 logo">
+               <a href="/"><img src="../img/logo.jpg" alt=""></a>
+            </div>
+            <div class="col-md-3">
+               <div class="bd-example justify-content-end">
+                  <address>
+                     <strong>Телефоны для связи</strong><br>
+                     <!--<abbr title="Phone">P:--></abbr><a href="tel:89051094200">8(905)109-42-00</a> <br>
+                     <!--<abbr title="Phone">P:--></abbr> <a href="tel:89605131295">8(960)513-12-95</a> <br>
+                     <!--<abbr title="Phone">P:--></abbr> <a href="tel:89968938074">8(996)893-80-74</a> <br>
+               </div>
+               </address>
+            </div>
+            <div class="col-md-3">
+               <address>
+                  <strong>Почта</strong><br>
+                  <a href="mailto:89109867353@mail.ru">89109867353@mail.ru</a> <br>
+                  <a href="#" class="btn btn-primary">Заказать звонок</a>
+               </address>
+            </div>
+         </div>
+      </div>
+      <div id="wriper">
+      <? include("../block/header.php")?>
+      <div id="centrinfo">
+      <div class="catalogNaw">
+         <span class="text1">Вы находитесь тут:</span>&nbsp;
+         <a href="/index.php">Главная</a>&nbsp;
+         <a href="/prf-145.php">Пресс-подборщик ПРФ-145</a>&nbsp;
+         <span><a href="/PRF-145/">Каталог запчастей  ПРФ-145</a></span>&nbsp;
+         <div class="topItem">
+            <div class="menuItem">
+               <ul>
+                  <li><a href="1.html" target="topItemFrame">Вал ведущий ПР-1,4.51.02.000</a></li>
+                  <li><a href="2.html" target="topItemFrame">Вал привода ПР-01.070А</a></li>
+                  <li><a href="3.html" target="topItemFrame">Вал ПР-1,4.50.05.000</a></li>
+                  <li><a href="4.html" target="topItemFrame">Вал ПР-1,4.01.03.000</a></li>
+                  <li><a href="5.html" target="topItemFrame">Привод ПР-13.030</a></li>
+                  <li><a href="6.html" target="topItemFrame">Пресс-подборщик ПРФ 145, ПРФ 110</a></li>
+                  <li><a href="7.html" target="topItemFrame">Ось ПР-1,4.03.02.000</a></li>
+                  <li><a href="8.html" target="topItemFrame">Пресс-подборщик ПРФ-145</a></li>
+               </ul>
+            </div>
+            <div class="menuItem-2">
+               <ul>
+                  <li><a href="9.html" target="topItemFrame">Основание камеры ПР-1,4.53.00.000</a></li>
+                  <li><a href="10.html" target="topItemFrame">Муфта предохранительная ПР-07.00.230</a></li>
+                  <li><a href="11.html" target="topItemFrame">Механизм прессующий для прф 145</a></li>
+                  <li><a href="12.html" target="topItemFrame">Лобовина ПР-1,4.50.00.000</a></li>
+                  <li><a href="13.html" target="topItemFrame">Колесо ПР-15.00.360</a></li>
+                  <li><a href="14.html" target="topItemFrame">Камера передняя, камера задняя</a></li>
+                  <li><a href="15.html" target="topItemFrame">Барабан ПР-1,4.53.04.000</a></li>
+                  <li><a href="16.html" target="topItemFrame">Каталог запчастей для пресс-подборщика</a></li>
+                  <li><a href="17.html" target="topItemFrame">Аппарат обматывающий ПР-1,4.01.04.000</a></li>
+               </ul>
+            </div>
+            <div id="topItemFrame">
+               <iframe id="top-ItemFrame" class="iframe_item" src="1.html" align="left" name="topItemFrame" align="right" scrolling="no" frameborder="1">
+               </iframe>
+            </div>
+         </div>
+      </div>
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+      <link rel="stylesheet" href="../css/main.min.css">
+      <script src="../js/scripts.min.js"></script>
+      <!-- Yandex.Metrika informer -->
+      <a href="https://metrika.yandex.ru/stat/?id=22174855&amp;from=informer"
+         target="_blank" rel="nofollow"><img src="https://informer.yandex.ru/informer/22174855/1_1_FFFFFFFF_EFEFEFFF_0_pageviews"
+         style="width:80px; height:15px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры)" /></a>
+      <!-- /Yandex.Metrika informer -->
+      <!-- Yandex.Metrika counter -->
+      <script type="text/javascript" >
+         (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+         m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+         (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+         
+         ym(22174855, "init", {
+            id:22174855,
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true,
+            webvisor:true
+         });
+      </script>
+      <noscript>
+         <div><img src="https://mc.yandex.ru/watch/22174855" style="position:absolute; left:-9999px;" alt="" /></div>
+      </noscript>
+      <!-- /Yandex.Metrika counter -->
+   </body>
+</html>
